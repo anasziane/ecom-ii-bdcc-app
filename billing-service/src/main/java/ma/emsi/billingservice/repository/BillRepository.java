1d8e2361ac5e01e32a9dfdd341f608c51861ddfe
kavi <kavi@192.168.1.10>
2025-11-10 20:20:39 +0100

    Initial commit of ecom-ii-bdcc-app project

--- billing-service/src/main/java/ma/emsi/billingservice/repository/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.emsi.billingservice.repository;
-
-import ma.emsi.billingservice.entities.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BillRepository extends JpaRepository<Bill, Long> {
-}
--- billing-service/src/main/java/ma/emsi/billingservice/repository/ProductItemRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.emsi.billingservice.repository;
-
-import ma.emsi.billingservice.entities.ProductItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
-}
